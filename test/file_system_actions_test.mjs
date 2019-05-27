43be96fd2e6e11846536c78278311b169779dbb4
James C Russell <james+git@botanicus.me>
2019-05-27 12:53:28 +0000

    More tests

--- test/file_system_actions_test.mjs
+++ test/file_system_actions_test.mjs
@@ -2,6 +2,24 @@ import test from 'ava'
 import assert from 'assert'
 import { FileSystemActions } from '../fs_actions.mjs'
 
-test('does something', t => {
+const list = [
+  {
+    validate: () => {},
+    message: () => {},
+    commit: () => {}
+  }
+]
+
+test('FileSystemActions can be instantiated without any arguments', t => {
+  assert.doesNotThrow(() => new FileSystemActions())
+  const actions = new FileSystemActions()
+  assert.strictEqual(actions.actions.length, 0)
+  t.pass()
+})
+
+test('FileSystemActions can be instantiated with a list of valid actions', t => {
+  assert.doesNotThrow(() => new FileSystemActions(...list))
+  const actions = new FileSystemActions(...list)
+  assert.strictEqual(actions.actions.length, list.length)
   t.pass()
 })
