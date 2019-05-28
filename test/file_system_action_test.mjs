b9ab79d062a42c86840adde6c381947ac2adcf98
James C Russell <james+git@botanicus.me>
2019-05-28 09:13:12 +0000

    FileSystemAction tests done

--- test/file_system_action_test.mjs
+++ test/file_system_action_test.mjs
@@ -2,6 +2,19 @@ import test from 'ava'
 import assert from 'assert'
 import { FileSystemAction } from '../'
 
-test('does something', t => {
+const action = new FileSystemAction()
+
+test('FileSystemAction.prototype.validate() throws a non implemented error', t => {
+  assert.throws(() => action.validate(), /Override this method/)
+  t.pass()
+})
+
+test('FileSystemAction.prototype.message() throws a non implemented error', t => {
+  assert.throws(() => action.message(), /Override this method/)
+  t.pass()
+})
+
+test('FileSystemAction.prototype.commit() throws a non implemented error', t => {
+  assert.throws(() => action.commit(), /Override this method/)
   t.pass()
 })
